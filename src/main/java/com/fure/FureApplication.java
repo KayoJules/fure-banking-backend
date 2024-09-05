494a79326d54967c488d598785ae9abc1d409091
KayoJules <ChaoticPhoenix777@gmail.com>
2024-09-04 19:58:00 -0500

    init

---
+++ src/main/java/com/fure/FureApplication.java
@@ -0,0 +1,13 @@
+package com.fure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fure/FureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
