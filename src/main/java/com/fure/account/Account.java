211fac622205e79519374dcd13ba303c44effc57
KayoJules <ChaoticPhoenix777@gmail.com>
2024-09-10 22:24:07 -0500

    Base class for Account, User

--- src/main/java/com/fure/account/Account.java
+++ src/main/java/com/fure/account/Account.java
@@ -1,4 +1,22 @@
 package com.fure.account;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "account")
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal balance;
 }
--- src/main/java/com/fure/user/User.java
+++ src/main/java/com/fure/user/User.java
@@ -1,4 +1,24 @@
 package com.fure.user;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    private String email;
+    private String password;
 }
