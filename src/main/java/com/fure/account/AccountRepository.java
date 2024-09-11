4d16c32e34d68b10f82c9e5e6a1c9638a27e19d2
KayoJules <ChaoticPhoenix777@gmail.com>
2024-09-10 22:16:00 -0500

    Repository layer for Account, User

--- src/main/java/com/fure/account/AccountRepository.java
+++ src/main/java/com/fure/account/AccountRepository.java
@@ -1,4 +1,8 @@
 package com.fure.account;
 
-public class AccountRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Integer> {
 }
