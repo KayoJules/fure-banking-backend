45bc226375584991ecc0c7b881fc4ae85f19e0c7
KayoJules <ChaoticPhoenix777@gmail.com>
2024-09-19 00:01:46 -0500

    Service annotations & constructors

--- src/main/java/com/fure/account/AccountService.java
+++ src/main/java/com/fure/account/AccountService.java
@@ -1,4 +1,17 @@
 package com.fure.account;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+
 }
